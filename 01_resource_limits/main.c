b7d1ab6f21b2f263d8b0d6eb610ffc13732efde4
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-06-26 20:12:58 +0300

    exit handler added

--- 01_resource_limits/main.c
+++ 01_resource_limits/main.c
@@ -12,64 +12,67 @@ int main(){
    //RLIMIT_NOFILE
    if(getrlimit(RLIMIT_NOFILE, &limit) < 0)
       fprintf(stderr, "%s\n", strerror(errno));
-   else
-      printf("%s","RLIMIT_NOFILE: \t");
-      
-   if (limit.rlim_cur == RLIM_INFINITY)
-      printf(" (infinity) ");
-   else
-      printf(" %d ", limit.rlim_cur);
+   else{
+      printf("%s","RLIMIT_NOFILE: \t");      
+      if (limit.rlim_cur == RLIM_INFINITY)
+         printf(" (infinity) ");
+      else
+         printf(" %d ", limit.rlim_cur);
 
-   if (limit.rlim_max == RLIM_INFINITY)
-      printf(" (infinity) \n");
-   else
-      printf(" %d \n", limit.rlim_max);
+      if (limit.rlim_max == RLIM_INFINITY)
+         printf(" (infinity) \n");
+      else
+         printf(" %d \n", limit.rlim_max);
+   }
    
    //RLIMIT_DATA   
    if (getrlimit(RLIMIT_DATA, &limit) < 0)
       fprintf(stderr, "%s\n", strerror(errno));
-   else
+   else{
       printf("%s","RLIMIT_DATA:\t");
+      if (limit.rlim_cur == RLIM_INFINITY)
+         printf(" (infinity) ");
+      else
+         printf("%d ", limit.rlim_cur);
 
-   if (limit.rlim_cur == RLIM_INFINITY)
-      printf(" (infinity) ");
-   else
-      printf("%d ", limit.rlim_cur);
-
-   if (limit.rlim_max == RLIM_INFINITY)
-      printf(" (infinity) \n");
-   else
-      printf("%d \n", limit.rlim_max); 
+      if (limit.rlim_max == RLIM_INFINITY)
+         printf(" (infinity) \n");
+      else
+         printf("%d \n", limit.rlim_max); 
+   }
    
    //RLIMIT_NPROC
    if (getrlimit(RLIMIT_NPROC, &limit) < 0)
       fprintf(stderr, "%s\n", strerror(errno));
-   else
+   else{
       printf("%s","RLIMIT_NPROC:\t");
+      if (limit.rlim_cur == RLIM_INFINITY)
+         printf(" (infinity) ");
+      else
+         printf(" %d ", limit.rlim_cur);
 
-   if (limit.rlim_cur == RLIM_INFINITY)
-      printf(" (infinity) ");
-   else
-      printf(" %d ", limit.rlim_cur);
-
-   if (limit.rlim_max == RLIM_INFINITY)
-      printf(" (infinity) \n");
-   else
-      printf(" %d \n", limit.rlim_max);      
+      if (limit.rlim_max == RLIM_INFINITY)
+         printf(" (infinity) \n");
+      else
+         printf(" %d \n", limit.rlim_max);  
+   }    
 
    //RLIMIT_CPU
-   getrlimit(RLIMIT_CPU, &limit);
-   printf("%s","RLIMIT_CPU:\t");
-   
-   if (limit.rlim_cur == RLIM_INFINITY)
-      printf(" (infinity) ");
-   else
-      printf("%d ", limit.rlim_cur);
+   if (getrlimit(RLIMIT_CPU, &limit) < 0)
+      fprintf(stderr, "%s\n", strerror(errno));
+   else{
+      printf("%s","RLIMIT_CPU:\t");
+      if (limit.rlim_cur == RLIM_INFINITY)
+	 printf(" (infinity) ");
+      else
+         printf("%d ", limit.rlim_cur);
 
-   if (limit.rlim_max == RLIM_INFINITY)
-      printf(" (infinity) \n");
-   else
-      printf("%d \n", limit.rlim_max);
+      if (limit.rlim_max == RLIM_INFINITY)
+         printf(" (infinity) \n");
+      else
+         printf("%d \n", limit.rlim_max);
+   }
+      
    return 0;        
 }	
 
---
+++ 02_exit_handler/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>  
+#include <stdlib.h>
+#include <unistd.h>
+
+void myexit(int exitStatus, void *arg){
+	printf("Exit Handler called with exit status=%d and arg=%ld\n", exitStatus, (long)arg);
+}
+ 
+int main(int argc, char *argv[]){
+	on_exit(myexit, (void *)7);
+	printf("This is main\n");
+	exit(54);
+}	
+
+
