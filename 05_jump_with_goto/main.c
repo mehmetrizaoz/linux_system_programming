ddf6c0d3db13ec8a01fb6278830e4a0b127b93d8
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-06-26 21:14:31 +0300

    goto statement

---
+++ 05_jump_with_goto/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+   printf("Start of loop.\n");
+   
+   int ctr=1;
+   
+   label1: 
+   printf ("PUCIT\n");
+   ctr++;
+
+   if (ctr == 6)
+      goto label2;
+
+   goto label1;
+
+   label2:
+   printf("Out of loop\n");
+
+   return 0;
+}
