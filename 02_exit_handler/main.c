cdad9efa1a3d4cca8c07cbe5f6d8b37e54034fb1
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-06-26 21:02:49 +0300

    processing command line arguments

--- 02_exit_handler/main.c
+++ 02_exit_handler/main.c
@@ -3,13 +3,12 @@
 #include <unistd.h>
 
 void myexit(int exitStatus, void *arg){
-	printf("Exit Handler called with exit status=%d and arg=%ld\n", exitStatus, (long)arg);
+   printf("Exit Handler called with exit status=%d and arg=%ld\n", exitStatus, (long)arg);
 }
- 
-int main(int argc, char *argv[]){
-	on_exit(myexit, (void *)7);
-	printf("This is main\n");
-	exit(54);
-}	
 
+int main(int argc, char *argv[]){
+   on_exit(myexit, (void *)7);
+   printf("This is main\n");
+   exit(54);
+}
 
