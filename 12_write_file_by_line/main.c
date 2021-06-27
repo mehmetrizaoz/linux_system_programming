fc1091754ad3cc3704a6884d69584ef24ac5c706
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-06-27 12:10:53 +0300

    write file by line

---
+++ 12_write_file_by_line/main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char * argv[]){
+   if(argc !=2){
+      printf("Invalid number of arguments. Please enter file name to display its contents...\n");
+      exit(1);
+   }
+
+   FILE* fp = fopen(argv[1], "a");
+   if (fp == NULL){
+      perror("fopen failed");
+      exit(1);
+   }
+   
+   char name[100];
+   printf("Enter your friend's name: ");
+   scanf("%[^\n]s", name);
+   fputs(name, fp);
+   fputc('\n',fp);	
+   fclose(fp);
+   printf("\nDone bye bye...\n");
+   
+   return 0;
+}
