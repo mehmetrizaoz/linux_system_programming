5f066aee839755714eca62503f24f107aa586aef
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-07-02 20:44:00 +0300

    data sending to dedicated terminal stdout

---
+++ 36_write_to_dedicated_terminal/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+   if (argc != 2 ){
+	   fprintf(stderr,"usage: ./a.out /dev/pts/n\n");
+	   exit(1);
+   }
+   
+   int fd = open(argv[1], 1);
+   if (fd == -1){
+	   perror("open() failed"); 
+      exit(1);
+   }
+   
+   char	buf[512];	/* loop until EOF on input */
+   while(fgets(buf, 512, stdin) != NULL )
+      if (write(fd, buf, strlen(buf)) == -1 )
+	      break;
+   
+   close(fd);
+   
+   return 0;
+}
