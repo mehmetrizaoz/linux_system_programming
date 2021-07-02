c1bd61f8428a67783e002a5853e50f6517165107
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-07-02 22:58:58 +0300

    get winsize

---
+++ 41_get_terminal_size_with_ioctl/main.c
@@ -0,0 +1,14 @@
+#include <sys/ioctl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    struct winsize wbuf;
+    if(ioctl(0, TIOCGWINSZ, &wbuf) == -1){
+        perror("Error in ioctl");
+        exit(1);
+    }
+    
+    printf("%d rows x %d cols \n", wbuf.ws_row, wbuf.ws_col);  
+    return 0;
+}
