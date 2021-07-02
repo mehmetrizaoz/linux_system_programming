057c5732248096e32adcd306c80ea9e5c151c81f
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-07-02 13:25:44 +0300

    indentation updates

--- 32_list_directories_and_files/main.c
+++ 32_list_directories_and_files/main.c
@@ -11,36 +11,37 @@ void do_ls(char*);
 
 int main(int argc, char* argv[]){
    if (argc == 1){
-         printf("Directory listing of pwd:\n");
+      printf("Directory listing of pwd:\n");
       do_ls(".");
    }
    else{
       int i = 0;
       while(++i < argc){
          printf("Directory listing of %s:\n", argv[i] );
-	 do_ls(argv[i]);
+	      do_ls(argv[i]);
       }
-   }
-   
+   }   
    return 0;
 }
 
 void do_ls(char * dir){
    struct dirent * entry;
-   DIR * dp = opendir(dir);
+   DIR * dp = opendir(dir);   
    if (dp == NULL){
       fprintf(stderr, "Cannot open directory:%s\n",dir);
       return;
    }
    errno = 0;
    while((entry = readdir(dp)) != NULL){
-         if(entry == NULL && errno != 0){
-  		perror("readdir failed");
-		exit(1);
-  	}else{
-                if(entry->d_name[0] == '.')
-                    continue;
-        }     	printf("%s\n",entry->d_name);
+      if(entry == NULL && errno != 0){
+  		   perror("readdir failed");
+		   exit(1);
+  	   }      
+      else{
+         if(entry->d_name[0] == '.')
+            continue;
+      }      
+      printf("%s\n",entry->d_name);
    }
    closedir(dp);
 }
