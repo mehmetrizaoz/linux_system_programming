ca19c39741929e3110268e4c8f0a9601ce3cce86
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-07-03 22:28:52 +0300

    child process status flags details

--- 48_wait_system_call/main.c
+++ 48_wait_system_call/main.c
@@ -19,10 +19,33 @@ int main(){
         int status;
         pid_t rv = wait (&status);
         printf ("Hello I m Parent.\n");
-        int low_8 = status & 0xff;
-        int high_8 = status >> 8;
-        if(low_8 == 0)
-   	        printf("Return value of wait is %ld, and status is %d\n",rv, high_8);        
+        disp_status(status);       
         exit(54);
     }
+}
+
+void disp_status(int status){
+    //normal termination
+    int low_7 = status & 0x7f;
+    int high_8 = status >> 8;
+    if(low_7 == 0)
+        printf("My child terminated normally with exit value %d\n", high_8);
+   
+    //killed by a signal
+    if(high_8 == 0)
+        printf("My child was killed by Signal Number = %d\n", low_7);
+        
+    int bit_7 = status & 0x80;
+    bit_7 = bit_7 >> 7;
+    if(bit_7 == 1)
+        printf("Core Dump file generated.\n");
+    
+    //stopped by a signal
+    if(low_7 == 0x7F)
+        printf("My child was stopped by Signal Number = %d\n", high_8);
+
+    //continued by SIGCONT
+    int low_16 = status & 0xffff;
+    if(low_16 == 0xffff)
+        printf("My child continued its execution due to SIGCONT\n");
 }
