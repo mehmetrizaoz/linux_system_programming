36041d8a10a47d088d89f81597667012708314ad
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-07-03 22:36:25 +0300

    vfork usage

---
+++ 49_vfork/main.c
@@ -0,0 +1,23 @@
+/*  Proves that after a vfork() call, child execute first and the parent 
+automatically waits, and starts its execution after the child terminates*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(){
+   int cpid = vfork();
+   if (cpid == 0){
+      sleep(1);
+      printf("Hello I am child..\n");
+      sleep(2);
+      printf("Hello I am child again, I am exitting.\n");
+      sleep(2);
+      exit(0);
+   }
+   else{
+      printf("\nHello I am parent, my child has terminated\n");
+      exit(0);
+   }
+}
+
