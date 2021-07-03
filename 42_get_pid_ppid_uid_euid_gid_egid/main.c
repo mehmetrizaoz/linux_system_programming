1da23575cc659c08945cbd0f8dbaa8638ca48f33
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-07-03 10:58:35 +0300

    get process ids

---
+++ 42_get_pid_ppid_uid_euid_gid_egid/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+    printf("My PID is: %ld\n", (long)getpid());
+    printf("My PPID is: %ld\n", (long)getppid());
+
+    printf("My Real user-ID is: %ld\n", (long)getuid());
+    printf("My Effective user-ID is: %ld\n", (long)geteuid());
+
+    printf("My Real group-ID is: %ld\n", (long)getgid());
+    printf("My Effective group-ID is: %ld\n", (long)getegid());
+
+   return 0;
+}
