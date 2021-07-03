81f52d594e7592dfb95ad217160204c944b40406
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-07-03 15:00:59 +0300

    zombie process

---
+++ 47_zombie/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(){
+    int cpid = fork();
+    if(cpid == 0){
+        printf("Terminating child with PID = %ld\n", (long)getpid());
+        exit (0);
+    }
+    else{
+        printf("Running parent, PID=%ld\n",(long)getpid());
+        while(1);
+    }
+    
+    return 0;
+}
