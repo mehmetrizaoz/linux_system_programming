a7a6ebf021cad636dbd09cf092242059bc70bb31
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-07-03 22:50:11 +0300

    list environment variables

---
+++ 53_list_environment_variables/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char **environ;
+int main(){
+   printf("\n Environment variable passed are:\n");
+   for (int i = 0; environ[i] != NULL ; i++)
+      printf("environ[%d]:%s\n", i, environ[i]);
+
+   return 0;
+}
