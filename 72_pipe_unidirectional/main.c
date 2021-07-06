880414b1d6bdfc52dc0e60be736d4be66751e86a
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-07-06 11:07:00 +0300

    pipe ipc directory name updated

---
+++ 72_pipe_unidirectional/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define SIZE 1024
+int main(){
+   int fd[2];
+   int rv = pipe(fd);
+//create a child process
+   pid_t cpid = fork();
+//parent code (parent is writer process)
+   if (cpid != 0) {
+//close read pipe
+      close(fd[0]);
+      const char * msg = "Welcome to Communication using pipes\n";
+      write(fd[1], msg,strlen(msg));
+      waitpid(cpid, NULL, 0);
+      exit(0);
+   }
+//child code (child is reader process)
+   else { 
+//close write pipe      
+      close(fd[1]);
+      char buff[SIZE];
+      memset(buff, '\0',SIZE);
+      fprintf(stderr,"Message sent from parent is: "); 
+      int n = read(fd[0],buff,SIZE);
+      write(1,buff,n);   
+      exit(0);
+   }
+}
