52cc83da2a8639acf76926b35d6de3f076c585f8
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-07-09 23:06:34 +0300

    mmap file mapping

--- 81_mmap_read_file/main.c
+++ 81_mmap_read_file/main.c
@@ -1,50 +1,62 @@
-#include <stdio.h>
-#include <sys/mman.h>
 #include <stdlib.h>
-#include <sys/stat.h>
+#include <stdio.h>
+#include <stdint.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]){
-    if(argc < 2){
-        printf("File path not mentioned\n");
-        exit(0);
+int main(int argc, const char *argv[])
+{    
+    const char *filepath = "file.bin";
+
+    int fd = open(filepath, O_RDONLY, (mode_t)0600);
+    
+    if (fd == -1)
+    {
+        perror("Error opening file for writing");
+        exit(EXIT_FAILURE);
+    }        
+    
+    struct stat fileInfo = {0};
+    
+    if (fstat(fd, &fileInfo) == -1)
+    {
+        perror("Error getting the file size");
+        exit(EXIT_FAILURE);
     }
-   
-    const char *filepath = argv[1];
-    int fd = open(filepath, O_RDONLY);
-    if(fd < 0){
-        printf("\n\"%s \" could not open\n",
-               filepath);
-        exit(1);
+    
+    if (fileInfo.st_size == 0)
+    {
+        fprintf(stderr, "Error: File is empty, nothing to do\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("File size is %ji\n", (intmax_t)fileInfo.st_size);
+    
+    char *map = mmap(0, fileInfo.st_size, PROT_READ, MAP_SHARED, fd, 0);
+    if (map == MAP_FAILED)
+    {
+        close(fd);
+        perror("Error mmapping the file");
+        exit(EXIT_FAILURE);
+    }
+    
+    for (off_t i = 0; i < fileInfo.st_size; i++)
+    {
+        printf("Found character %c at %ji\n", map[i], (intmax_t)i);
+    }
+    
+    // Don't forget to free the mmapped memory
+    if (munmap(map, fileInfo.st_size) == -1)
+    {
+        close(fd);
+        perror("Error un-mmapping the file");
+        exit(EXIT_FAILURE);
     }
 
-    struct stat statbuf;
-    int err = fstat(fd, &statbuf);
-    if(err < 0){
-        printf("\n\"%s \" could not open\n",
-                       filepath);
-        exit(2);
-    }
-
-	//int fsize = lseek(fd, 0, SEEK_END);
-    char *ptr = mmap(NULL,statbuf.st_size,PROT_WRITE, MAP_PRIVATE,fd,0);
-    if(ptr == MAP_FAILED){
-        printf("Mapping Failed\n");
-        return 1;
-    }
+    // Un-mmaping doesn't close the file, so we still need to do that.
     close(fd);
-
-    ssize_t n = write(1,ptr,statbuf.st_size);
-    if(n != statbuf.st_size){
-        printf("Write failed");
-    }  
-
-    err = munmap(ptr, statbuf.st_size);
-
-    if(err != 0){
-        printf("UnMapping Failed\n");
-        return 1;
-    }
+    
     return 0;
 }
