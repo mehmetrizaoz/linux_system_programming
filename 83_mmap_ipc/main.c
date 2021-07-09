7b04724600c7d349069d2ca719ce6eb867c52345
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-07-09 23:15:25 +0300

    mmap ipc via file

---
+++ 83_mmap_ipc/main.c
@@ -0,0 +1,48 @@
+/*
+*  Video Lecture: 30
+*  Programmer: Arif Butt
+*  Course: System Programming with Linux
+*  Program mmaps an entire file in its address space
+*  and then do a fork, both the child and parent
+*  displays contents of file on stdout
+*  usage: ./mmap2
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
+
+int main(int argc, char* argv[]){
+//open a file
+   int fd = open("file.txt", O_RDWR);
+//get attributes of the file
+   struct stat sbuff;
+   fstat(fd, &sbuff);
+   int fsize = sbuff.st_size;
+//memory map the file
+   char* filedata = mmap(NULL, fsize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+   
+//create a child process
+   pid_t cpid = fork();
+   if(cpid==0){
+      printf("Child access memory mapped file: %s\n", filedata);
+      filedata[0] = 'k';
+      munmap(filedata, fsize);
+      close(fd);
+      exit(0);
+   }
+   else{
+      waitpid(cpid, NULL,0);
+      printf("Parent access memory mapped file: %s\n", filedata);
+      munmap(filedata, fsize);
+      close(fd);
+      exit(0);
+   }
+   return 0;
+}
